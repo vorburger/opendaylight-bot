1ba2abfb02b9562ce4f6b4f94a02e2007758f404
Michael Vorburger <vorburger@redhat.com>
2018-05-02 23:14:41 +0200

    BotServlet now actually uses the Bot

--- spring/src/main/java/org/opendaylight/bot/spring/web/BotServlet.java
+++ spring/src/main/java/org/opendaylight/bot/spring/web/BotServlet.java
@@ -7,21 +7,37 @@
  */
 package org.opendaylight.bot.spring.web;
 
+import com.google.common.base.Strings;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.opendaylight.bot.Bot;
+import org.opendaylight.bot.BotException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Bot's Servlet.
  *
  * @author Michael Vorburger.ch
  */
+@SuppressWarnings("serial")
+@SuppressFBWarnings({"SE_NO_SERIALVERSIONID", "SE_BAD_FIELD"})
 public class BotServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(BotServlet.class);
+
+    private final Bot bot;
+
+    @Inject
+    public BotServlet(Bot bot) {
+        this.bot = bot;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,6 +45,16 @@ public class BotServlet extends HttpServlet {
 
         response.setContentType("text/plain");
         PrintWriter out = response.getWriter();
-        out.println("Hello World!");
+        String topicName = request.getParameter("topic");
+
+        try {
+            if (!Strings.isNullOrEmpty(topicName)) {
+                bot.topic(topicName);
+            } else {
+                out.println("USAGE: /bot?topic={managedTopicName}");
+            }
+        } catch (BotException e) {
+            LOG.error("Bot failed", e);
+        }
     }
 }
