71c3e3d56e38c0327d28bac905b89551666620c5
Michael Vorburger <vorburger@redhat.com>
2018-05-02 22:14:21 +0200

    simplest possible Servlet with Spring and test

--- spring/src/main/java/org/opendaylight/bot/spring/Application.java
+++ spring/src/main/java/org/opendaylight/bot/spring/Application.java
@@ -1,12 +1,23 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.bot.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@SuppressWarnings("HideUtilityClassConstructor")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
--- spring/src/test/java/org/opendaylight/bot/spring/ApplicationTests.java
+++ spring/src/test/java/org/opendaylight/bot/spring/ApplicationTests.java
@@ -1,16 +1,38 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.bot.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)// (classes = BotSpringConfiguration.class)
 public class ApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test public void contextLoads() {
+    }
+
+    @Test public void httpGetSlashBot() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/bot", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo("Hello World!\n");
     }
 
 }
