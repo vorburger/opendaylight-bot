3d77e7191db0e83720f87785314ed198eac4391f
Prateek Chanda <prateekkol21@gmail.com>
2018-05-23 17:53:58 +0530

    check identantion and lexical order

--- bot/src/main/java/org/opendaylight/bot/jenkins/Client.java
+++ bot/src/main/java/org/opendaylight/bot/jenkins/Client.java
@@ -7,48 +7,47 @@
  */
 package org.opendaylight.bot.jenkins;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.google.common.collect.Lists;
-
-import com.cdancy.jenkins.rest.JenkinsClient;
 
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.cdancy.jenkins.rest.JenkinsClient;
 
 /**
-* Template of Initial Jenkins Client API
+* Template of Initial Jenkins Client API.
 * @author Prateek Chanda
 */
 
 public class Client {
-  public void connect() {
+
+    public void connect() {
     
-    JenkinsClient client = JenkinsClient.builder()
-      .endPoint("http://127.0.0.1:8080")
-      .credentials("admin:password") 
-      .build();
-    
-  }
+        JenkinsClient client = JenkinsClient.builder()
+            .endPoint("http://127.0.0.1:8080")
+            .credentials("admin:password") 
+            .build();
+    }
     
     public void Buildwithparameters() {
    
-      MockWebServer server = mockWebServer();
+        MockWebServer server = mockWebServer();
 
-      server.enqueue(
-            new MockResponse().setHeader("Location", "http://127.0.1.1:8080/job/item/1/").setResponseCode(201));
-      JenkinsApi jenkinsApi = api(server.getUrl("/"));
-      JobsApi api = jenkinsApi.jobsApi();
+        server.enqueue(
+              new MockResponse().setHeader("Location", "http://127.0.1.1:8080/job/item/1/").setResponseCode(201));
+        JenkinsApi jenkinsApi = api(server.getUrl("/"));
+        JobsApi api = jenkinsApi.jobsApi();
       
-      Map<String, List<String>> params = new HashMap<>();
-      params.put("Key", Lists.newArrayList("KeyValue"));
-      IntegerResponse output = api.buildWithParameters(null, "JobName", params);
+        Map<String, List<String>> params = new HashMap<>();
+        params.put("Key", Lists.newArrayList("KeyValue"));
+        IntegerResponse output = api.buildWithParameters(null, "JobName", params);
       
-      jenkinsApi.close();
-      server.shutdown();
+        jenkinsApi.close();
+        server.shutdown();
     
     }
       
