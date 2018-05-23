530e59862f17e8adeaf5b123df147da5eb526c2b
Prateek Chanda <prateekkol21@gmail.com>
2018-05-23 16:24:56 +0530

    building jobs with parameters

--- bot/src/main/java/org/opendaylight/bot/jenkins/Client.java
+++ bot/src/main/java/org/opendaylight/bot/jenkins/Client.java
@@ -2,18 +2,49 @@
 
 package org.opendaylight.bot.jenkins;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+
 import com.cdancy.jenkins.rest.JenkinsClient;
+
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+
 
 /**
 * Initial Jenkins Client API 
 */
 public class Client {
-  public static void main(String []args) {
+  public void connect() {
+    
     JenkinsClient client = JenkinsClient.builder()
-    .endPoint("http://127.0.0.1:8080")
-    .credentials("admin:password") 
-    .build();
+      .endPoint("http://127.0.0.1:8080")
+      .credentials("admin:password") 
+      .build();
     
   }
+    
+    public void Buildwithparameters() {
+   
+      MockWebServer server = mockWebServer();
+
+      server.enqueue(
+            new MockResponse().setHeader("Location", "http://127.0.1.1:8080/job/item/1/").setResponseCode(201));
+      JenkinsApi jenkinsApi = api(server.getUrl("/"));
+      JobsApi api = jenkinsApi.jobsApi();
+      
+      Map<String, List<String>> params = new HashMap<>();
+      params.put("Key", Lists.newArrayList("KeyValue"));
+      IntegerResponse output = api.buildWithParameters(null, "JobName", params);
+      
+      jenkinsApi.close();
+      server.shutdown();
+    
+    }
+      
+  
   
 }
